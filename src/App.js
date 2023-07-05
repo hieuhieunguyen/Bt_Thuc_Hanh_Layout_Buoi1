86e304fb32b6cf2794d577046fff52038128c4a6
Hieu <nguyenhuuhieu3523@gmail.com>
2023-07-05 21:37:46 +0700

    hoan thanh bai tap React layout buoi 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import BTThucHanhLayout from "./BTThucHanhLayout/BTThucHanhLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <BTThucHanhLayout />
+    </div>
+  );
+}
+
+export default App;
